9816fdd3521e17f433e79adf4282440d78c19c10
agostinhobritojr <agostinhobritojr@gmail.com>
2016-02-11 07:20:30 -0300

    pdi

---
+++ tutoriais/pdi/exemplos/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cv.h>
+#include <highgui.h>
+
+using namespace cv;
+
+int main(int argc, char** argv){
+  Mat image;
+  int width, height;
+  image = imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+  imshow("image", image);
+  waitKey();
+  return 0;
+}
