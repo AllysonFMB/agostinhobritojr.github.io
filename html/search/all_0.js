11de96f703b0d5dd25977268cb3cb52ca3744f19
agostinhobritojr <agostinhobritojr@gmail.com>
2017-10-25 11:53:41 -0300

    documentacao

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['testedca0201',['testedca0201',['../md_README.html',1,'']]]
+  ['fat',['fat',['../funcoes_8c.html#ae8d629c1cb76ec2f2979c06101631cf3',1,'fat(long x):&#160;funcoes.c'],['../funcoes_8h.html#ae8d629c1cb76ec2f2979c06101631cf3',1,'fat(long x):&#160;funcoes.c']]],
+  ['fatrec',['fatrec',['../funcoes_8c.html#a286320c527c20c2562f0d12985e0d9f7',1,'fatrec(long x):&#160;funcoes.c'],['../funcoes_8h.html#a286320c527c20c2562f0d12985e0d9f7',1,'fatrec(long x):&#160;funcoes.c']]],
+  ['funcoes_2ec',['funcoes.c',['../funcoes_8c.html',1,'']]],
+  ['funcoes_2eh',['funcoes.h',['../funcoes_8h.html',1,'']]]
 ];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['testedca0201',['testedca0201',['../md_README.html',1,'']]]
-];
